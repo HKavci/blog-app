56a4cb2b8a2a23838cd32f31b0c3f84a5aa728da
HakanKavci <hackankavci0@gmail.com>
2023-04-01 00:03:49 +0300

    started

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import PrivateRouter from "./PrivateRouter";
+import Details from "../pages/Details";
+import Footer from "../components/Footer";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Profile from "../pages/Profile";
+import NewBlog from "../pages/NewBlog";
+import NotFound from "../pages/NotFound";
+
+const AppRouter = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<Details />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/newblog" element={<NewBlog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default AppRouter;
