bff8fac89b73f4f2f43182ecd447da952449350a
HakanKavci <hackankavci0@gmail.com>
2023-04-10 13:58:50 +0300

    default profile photo added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -34,7 +34,7 @@ export default function Register() {
             first_name: "",
             last_name: "",
             email: "",
-            image: "",
+            image: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
             bio: "",
             password: "",
             password2: "",
