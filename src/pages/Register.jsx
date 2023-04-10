f788414e2688bb3b40e97dfc6e825fcd8865917e
HakanKavci <hackankavci0@gmail.com>
2023-04-10 14:00:07 +0300

    default bio added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,7 +35,7 @@ export default function Register() {
             last_name: "",
             email: "",
             image: "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg",
-            bio: "",
+            bio: "User",
             password: "",
             password2: "",
           }}
